5ab6d298d0236977021f95c130510158df8c3aaa
Edilson Firmino <edfirmin@c1r6p3.42nice.fr>
2024-11-06 15:37:52 +0100

    save getUser v1

--- srcs/front/frontend/src/pages/Profil.jsx
+++ srcs/front/frontend/src/pages/Profil.jsx
@@ -1,13 +1,23 @@
 import "../styles/Profil.css"
 import api from "../api";
+import { useState } from "react";
 
 function Profil() {
-    // const getUserBrute = () => {
-    //     const response = api.get("api/user/getUser/?asd");
-    //     return (response);
-    // }
-    // const user = getUserBrute()
-    // console.log(user)
+    const [user, setUser] = useState([])
+
+    const getUser = async () => {
+        const response = await api.get("/api/user/getUser/?asd")
+        return (response.data)
+    }
+    
+    const inituser = async () => {
+        const TMPuser = await getUser()
+        setUser(TMPuser);
+    }
+
+    // inituser()
+    console.log("data", user)
+
 
     return (
         <section className="bg-profil">
